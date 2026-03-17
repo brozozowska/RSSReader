96c520d3f0fafd780544fa71936c09483e1fcfce
brozozowska <s_rozov@me.com>
2026-03-17 17:55:29 +0300

    Initial Commit

---
+++ RSSReader/RSSReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSReaderApp.swift
+//  RSSReader
+//
+//  Created by Сергей Розов on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct RSSReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
